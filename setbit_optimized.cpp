65c4645c4e7ec61e40059a7c8f3bfb77f1298df2
TriptSudhakar <sudhakartript@gmail.com>
2022-12-30 11:18:53 +0530

    added files

---
+++ setbit_optimized.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int count = 0;
+    while(n)
+    {
+        int p = n & (-n); // extracts the first setbit from the right
+        n = n-p;
+        count++;
+        cout<<"Iteration no "<<count<<endl;
+    }
+    cout<<"Setbits : "<<count<<endl;
+    return 0;
+}
