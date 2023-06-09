5f4f997677b330f80792683ddd5a69e2dff1e24b
TriptSudhakar <sudhakartript@gmail.com>
2023-06-09 13:55:05 +0200

    added changes

--- histogram.cpp
+++ histogram.cpp
@@ -1,61 +1,57 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+#define ll long long int
 using namespace std;
-
-int getMaxArea(int hist[], int n)
-{
-	stack<int> s;
-
-	int max_area = 0; // Initialize max area
-	int tp; // To store top of stack
-	int area_with_top; // To store area with top bar
-					// as the smallest bar
-
-	int i = 0;
-	while (i < n) {
-		// If this bar is higher than the bar on top stack, push it to stack
-		if (s.empty() || hist[s.top()] <= hist[i])
-			s.push(i++);
-
-		// If this bar is lower than top of stack,
-		// then calculate area of rectangle with stack
-		// top as the smallest (or minimum height) bar.
-		// 'i' is 'right index' for the top and element
-		// before top in stack is 'left index'
-		else {
-			tp = s.top(); // store the top index
-			s.pop(); // pop the top
-
-			// Calculate the area with hist[tp] stack as smallest bar
-			area_with_top = hist[tp] * (s.empty() ? i : i - s.top() - 1);
-
-			// update max area, if needed
-			if (max_area < area_with_top) max_area = area_with_top;
-		}
-	}
-
-	while (s.empty() == false) {
-		tp = s.top();
-		s.pop();
-		area_with_top
-			= hist[tp] * (s.empty() ? i : i - s.top() - 1);
-
-		if (max_area < area_with_top)
-			max_area = area_with_top;
-	}
-
-	return max_area;
-}
-
-// Driver code
 int main()
 {
-	int n;
+	ll n;
 	cin>>n;
-	int hist[n];
-	for(int i=0;i<n;i++)
-	{
-		cin>>hist[i];
+	ll arr[n];
+	for(ll i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+	//left limit
+	stack <int> s;
+	ll left[n];
+    left[0] = -1;
+    s.push(0);
+	for(ll i=1;i<n;i++){
+		ll curr=arr[i];
+		while(!s.empty() && arr[s.top()] >= curr){
+			s.pop();
+		}
+		if(s.empty()){
+            left[i] = -1;
+        }
+		else{
+            left[i]=s.top();
+        }
+		s.push(i);
 	}
-	cout << getMaxArea(hist, n);
+
+	//right limit
+	stack <int> p;
+	ll right[n];
+    right[n-1] = n;
+    p.push(n-1);
+	for(ll i=n-2;i>=0;i--){
+		ll curr=arr[i];
+		while(!p.empty() && arr[p.top()]>=curr){
+			p.pop();
+		}
+		if(p.empty()){
+            right[i]=n;
+        }
+		else{
+            right[i]=p.top();
+        }
+		p.push(i);
+	}
+
+	ll mx = 0;
+	for(ll i=0;i<n;i++){
+		mx=max(mx,(right[i]-left[i]-1)*arr[i]);
+	}
+	cout<<mx;
 	return 0;
 }
