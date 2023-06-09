a718eb49f0cb6b5a6c050112d6d5749a75abd8f2
TriptSudhakar <sudhakartript@gmail.com>
2023-06-09 15:55:41 +0200

    added file

---
+++ next_greater_element.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+using namespace std;
+
+vector<int> nextGreaterElements(vector<int> &nums)
+{
+    int n = nums.size();
+    vector<int> res(n);
+
+    stack<int> s;
+
+    for (int i = 2 * n - 1; i >= 0; i--)
+    {
+
+        while (!s.empty() && nums[s.top()] <= nums[i % n])
+        {
+            s.pop();
+        }
+
+        res[i % n] = (s.empty() ? -1 : nums[s.top()]);
+        s.push(i % n);
+    }
+
+    return res;
+}
+
+int main() {
+    int n;
+    cin>>n;
+    vector<int> v(n);
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+    vector<int> ans = nextGreaterElements(v);
+
+    for(auto x:ans){
+        cout<<x<<" ";
+    }
+}
